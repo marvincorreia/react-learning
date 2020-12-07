9817615b364976cc6b56e696c2241e315d01156e
marvincorreia <marvin.correia@devgo.org>
2020-12-07 18:09:54 -0100

    creating routes and pages

---
+++ bootcamp-react/rocketshoes/src/routes.js
@@ -0,0 +1,12 @@
+import { Switch, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Home from './pages/Card'
+
+function Routes() {
+  return (
+    <div></div>
+  )
+}
+
+export default Routes;
