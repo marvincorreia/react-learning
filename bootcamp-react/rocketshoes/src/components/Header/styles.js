2d8649443e026a1ffb1c81baad55833fd5ee7d13
marvincorreia <marvin.correia@devgo.org>
2020-12-09 10:14:23 -0100

    Rocketshoes updated

--- bootcamp-react/rocketshoes/src/components/Header/styles.js
+++ bootcamp-react/rocketshoes/src/components/Header/styles.js
@@ -6,6 +6,11 @@ export const Container = styled.header`
   justify-content: space-between;
   align-items: center;
   margin: 50px 0;
+  img {
+    max-width: 100%;
+    max-height: auto;
+    min-width: 100px;
+  }
 `;
 
 export const Cart = styled(Link)`
@@ -13,23 +18,32 @@ export const Cart = styled(Link)`
   align-items: center;
   text-decoration: none;
   transition: opacity 0.2s;
-
   &:hover {
     opacity: 0.7;
   }
-
   div {
     text-align: right;
     margin-right: 10px;
-
-    strong: {
+    strong {
       display: block;
       color: #fff;
     }
-
     span {
       font-size: 12px;
       color: #999;
+    }
+  }
+  svg {
+    max-width: 100%;
+    min-width: 20px;
+  }
+  @media only screen and (max-width: 470px) {
+    & {
+      flex-direction: column;
+      div {
+        text-align: center;
+        margin: 0px;
+      }
     }
   }
 `;
